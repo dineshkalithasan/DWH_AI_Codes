4fd0447963741dcd58841ea9c22644503273c3ed
dineshkalithasan <dineshkalithasan@gmail.com>
2026-08-22 11:38:19 +0530

    removed files

--- test1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd;
-data = {
-    'apples': [3, 2, 0, 1], 
-    'oranges': [0, 3, 7, 2]
-}
-
-df=pd.DataFrame(data)
-print(df)
--- test3.py
+++
@@ -1,7 +0,0 @@
-original_list = [10, 12, 17, 29, 4, 6]
-new_list = []
-
-for num in original_list:
-    if num % 2 == 0:
-        new_list.append(num)
-print(new_list)
